4a2108c3a73f651a3855cd68ae61c333ff9a9c91
luna <luna@hackclub.app>
2025-04-02 10:40:32 +0800

    wow layout wow clean code wow!

--- src/constructors.rs
+++ src/constructors.rs
@@ -1,50 +1,72 @@
 use std::rc::Rc;
 
 use ratatui::{
-    layout::{Constraint, Direction, Layout, Margin, Rect},
-    style::{Color, Style},
-    widgets::{Block, Borders, LineGauge, List, ListItem, Paragraph},
-    Terminal,
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::{Block, Borders},
+    Frame,
 };
 
-pub fn mainlayout(area: Rect) -> (Rc<[Rect]>, Rc<[Rect]>, Rc<[Rect]>) {
-    let mainlayout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Percentage(100),
-            Constraint::Min(3)
-        ])
-        .split(area);
-
-    let upperlayout = Layout::default()
+pub fn construct(area: Rect) -> (Rect, Rect, Rect, Rect, Rect, Rect) {
+    let verticalchunks = Layout::default()
             .direction(Direction::Vertical)
             .constraints([
-                Constraint::Percentage(30), // playlist
-                Constraint::Percentage(45), // tracks
-                Constraint::Percentage(25)  // queue
+                Constraint::Percentage(100),
+                Constraint::Min(3)
             ])
-            .split(mainlayout[0]);
+            .split(area);
 
-    let lowerlayout = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([
-                    Constraint::Length(20),
-                    Constraint::Percentage(50),
-                    Constraint::Min(10)
-                ])
-                .split(mainlayout[1]);
+    let toplayout = verticalchunks[0];
+    let bottomlayout = verticalchunks[1];
 
-    (upperlayout, lowerlayout, mainlayout)
-}
+    let topchunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Fill(1),            // playlists
+                Constraint::Fill(3),            // tracks
+                Constraint::Fill(1)             // queue
+            ])
+            .split(toplayout);
 
-pub fn upperview(area: Rect, upperlayout: Rc<[Rect]>) {
+    let bottomchunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Length(20),         // controls
+                Constraint::Percentage(70),     // song name
+                Constraint::Min(10)             // progress bar
+            ])
+            .split(bottomlayout);
+    
+    let playlists = topchunks[0];
+    let tracks = topchunks[1];
+    let queue = topchunks[2];
+
+    let controls = bottomchunks[0];
+    let songname = bottomchunks[1];
+    let progressbar = bottomchunks[2];
+
+    (playlists, tracks, queue, controls, songname, progressbar)
     
 }
 
-pub fn lowerview(area: Rect, lowerlayout: Rc<[Rect]>) {
-    
+pub fn upperview(frame: &mut Frame, upperlayout: Rc<[Rect]>) {
+    frame.render_widget(Block::default().title(" playlists ").borders(Borders::ALL), upperlayout[0]);
+    frame.render_widget(Block::default().title(" tracks ").borders(Borders::ALL), upperlayout[1]);
+    frame.render_widget(Block::default().title(" queue ").borders(Borders::ALL), upperlayout[2]);
 }
 
-pub fn mainview(area: Rect, mainlayout: Rc<[Rect]>) {
+pub fn lowerview(frame: &mut Frame, lowerlayout: Rc<[Rect]>) {
+    frame.render_widget(Block::default().title(" controls ").borders(Borders::ALL), lowerlayout[0]);
+    frame.render_widget(Block::default().title(" song name ").borders(Borders::ALL), lowerlayout[1]);
+    frame.render_widget(Block::default().title(" progress bar ").borders(Borders::ALL), lowerlayout[2]);
+}
+
+pub fn drawmainview(frame: &mut Frame, areas: (Rect, Rect, Rect, Rect, Rect, Rect)) {
+    let (playlists, tracks, queue, controls, songinfo, progressbar) = areas;
     
+    frame.render_widget(Block::default().title(" playlists ").borders(Borders::ALL), playlists);
+    frame.render_widget(Block::default().title(" tracks ").borders(Borders::ALL), tracks);
+    frame.render_widget(Block::default().title(" queue ").borders(Borders::ALL), queue);
+    frame.render_widget(Block::default().title(" controls ").borders(Borders::ALL), controls);
+    frame.render_widget(Block::default().title(" song info ").borders(Borders::ALL), songinfo);
+    frame.render_widget(Block::default().title(" progress bar ").borders(Borders::ALL), progressbar);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::{
-    clone, error::Error, io, process::{
-        Child, Command, Stdio
-    }, 
-    rc::Rc, time::{Duration, Instant}
+    io, process::Child, 
+    time::Duration
 };
 
 use crossterm::{
@@ -11,10 +9,8 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout, Margin, Rect},
-    style::{Color, Style},
-    widgets::{Block, Borders, LineGauge, List, ListItem, Paragraph},
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
     Terminal,
 };
 // use tui_framework_experiment::{Button, ButtonState, ButtonTheme};
@@ -41,61 +37,82 @@ struct Queue {
 
 struct App {
     running: bool,
+    playing: bool,
     playlists: Vec<Playlist>,
     queue: Queue,
     currentqueidx: u16,
     currentplaylist: Playlist,
     shuffle: bool,
     repeat: bool,
-    mpv: Child
+    mpv: Option<Child>
 }
 
 impl App {
-    fn ontick(&mut self) {
-        
+    fn ontick(&mut self) -> Result<()> {
+        if self.playing {
+            if self.mpv.is_none() {
+                self.mpv = Some(std::process::Command::new("mpv").spawn()?);
+            }
+        }
+
+        Ok(())
     }
 
     fn onkey(&mut self, key: KeyCode) {
         match key {
             KeyCode::Char('q') => self.running = false,
+            KeyCode::Char(' ') => self.playing = !self.playing,
             _ => {}
         }
 
     }
 }
 
-fn draw(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>, app: &mut App) -> Result<()> {
+fn draw(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>, _app: &mut App) -> Result<()> {
     terminal.draw(|frame| {
-        let area = frame.area();
-        let (upperlayout, lowerlayout, mainlayout) = constructors::mainlayout(area);
-        constructors::upperview(area, upperlayout);
-        constructors::lowerview(area, lowerlayout);
-        constructors::mainview(area, mainlayout);
+        constructors::drawmainview(frame, constructors::construct(frame.area()))
     })?;
 
     Ok(())
 }
 
 fn main() -> Result<()> {
+    // --- setup terminal ---
     enable_raw_mode()?;
-    execute!(io::stdout(), EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()))?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?; 
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // --- initialize app ---
     let mut app = App {
         running: true,
+        playing: false,
         playlists: Vec::new(),
         queue: Queue { tracks: Vec::new() },
         currentqueidx: 0,
         currentplaylist: Playlist { name: String::new(), tracks: Vec::new() },
         shuffle: false,
         repeat: false,
-        mpv: Command::new("mpv").arg("--no-video").arg("--no-terminal").spawn()?
+        mpv: None // dont init mpv yet, only start when user starts playing music
     };
     
-    loop {
+    // --- main loop ---
+    while app.running {
         draw(&mut terminal, &mut app)?;
 
-    }
+        if event::poll(Duration::from_millis(250))? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    app.onkey(key.code);
+                }
+            }
+        }
 
+        app.ontick();
+    }
+    
+    // -- cleanup ---
     execute!(io::stdout(), LeaveAlternateScreen)?;
     disable_raw_mode()?;
     Ok(())
